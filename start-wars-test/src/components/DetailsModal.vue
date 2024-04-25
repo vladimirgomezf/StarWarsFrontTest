478fb080d14280494e1be1ef7c0f09f1ad03b032
Vlad Gómez <vladimirg.fumero@gmail.com>
2024-04-25 16:00:05 -0400

    show the movies

--- start-wars-test/src/components/DetailsModal.vue
+++ start-wars-test/src/components/DetailsModal.vue
@@ -2,6 +2,7 @@
 import {computed, onUpdated, ref} from "vue";
 import type {Person} from "@/components/starWarsCRUD.vue";
 import {EditOutlined} from "@ant-design/icons-vue";
+import apiServer from "../server/startWars"
 
 interface Props {
   show: boolean,
@@ -11,6 +12,7 @@ const props = defineProps<Props>()
 const emit = defineEmits(['close', 'edit'])
 
 const person = ref<Person>()
+const films = ref<any[]>([])
 
 onUpdated(async () => {
   person.value = {
@@ -24,6 +26,7 @@ onUpdated(async () => {
     gender: props.data.gender,
     films: props.data.films
   }
+  getFilms(person.value.films)
 })
 const viewModal = computed( () => props.show)
 const closeModal = () => {
@@ -31,6 +34,21 @@ const closeModal = () => {
 }
 const changeToEdit = (person: Person) => {
   emit('edit', person)
+}
+const getFilms = async (values: string[]) => {
+  films.value = []
+  values?.forEach(async film => {
+    var splited = film.split("/");
+    console.log("splited -> ", splited)
+    var index = splited.findIndex(item => item === "films");
+    var filmNumber = splited[index + 1];
+    console.log("filmNumber -> ", filmNumber)
+    if (filmNumber) {
+      const response = await apiServer.getFilms(filmNumber)
+      console.log("response -> ", response)
+      if (response) films.value.push(response)
+    }
+  })
 }
 </script>
 
@@ -48,23 +66,32 @@ const changeToEdit = (person: Person) => {
         <a-button key="submit" type="default" @click="closeModal">Close</a-button>
       </template>
       <a-card hoverable>
-        <h1>{{person.name ?? ""}}</h1>
+        <h1>{{person?.name ?? ""}}</h1>
         <div class="inlineData">
           <div class="column">
-            <div>Height: {{ person.height }}</div>
-            <div>Mass: {{ person.mass }}</div>
-            <div>Hair Color: {{ person.hairColor }}</div>
-            <div>Skin Color: {{ person.skinColor }}</div>
+            <div>Height: {{ person?.height }}</div>
+            <div>Mass: {{ person?.mass }}</div>
+            <div>Hair Color: {{ person?.hairColor }}</div>
+            <div>Skin Color: {{ person?.skinColor }}</div>
           </div>
           <div class="column">
-            <div>Eye Color: {{ person.eyeColor }}</div>
-            <div>Birth Year: {{ person.birthYear }}</div>
-            <div>Gender: {{ person.gender }}</div>
+            <div>Eye Color: {{ person?.eyeColor }}</div>
+            <div>Birth Year: {{ person?.birthYear }}</div>
+            <div>Gender: {{ person?.gender }}</div>
           </div>
         </div>
         <template #actions>
           <EditOutlined key="edit" @click="changeToEdit(person)"/>
         </template>
+        <br>
+        <a-card>
+        <h1>Movies:</h1>
+        <div class="inlineData">
+          <div class="column2" v-for="film in films.sort((a, b) => a.release_date - b.release_date)" :key="film">
+           Episode {{ film.episode_id }} - {{ film.title }} ({{ film.release_date }})
+          </div>
+        </div>
+      </a-card>
       </a-card>
     </a-modal>
   </div>
@@ -75,8 +102,10 @@ const changeToEdit = (person: Person) => {
   display: flex;
   flex-wrap: wrap;
 }
-
 .column {
   flex: 0 0 50%;
+}
+.column2 {
+  flex: 0 0 100%;
 }
 </style>
--- start-wars-test/src/server/startWars.ts
+++ start-wars-test/src/server/startWars.ts
@@ -3,5 +3,8 @@ import api from "./core"
 export default {
   getPeople(page: number){
     return api.get("people/?page=" + page)
+  },
+  getFilms(film: string){
+    return api.get("films/"+film)
   }
 }
